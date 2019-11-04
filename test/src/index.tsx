a4c2221d7ebb1e98c07824ff1058545f7f6efc42
Paul Henschel <drcmda@gmail.com>
2019-11-04 10:51:19 +0100

    use refs instead of useState for ref tracking to kill double render on startup

--- test/src/index.tsx
+++ test/src/index.tsx
@@ -1,8 +1,7 @@
-import ReactDOM from 'react-dom'
-import React, { Fragment, useEffect, useRef } from 'react'
-import useMeasure, { RectReadOnly } from 'react-use-measure'
-import { useSpring, animated as a } from 'react-spring'
-import { Global, Box, ScrollArea, ScrollContent } from './styles'
+import React, { Fragment, useState } from "react"
+import ReactDOM from "react-dom"
+import useMeasure from "react-use-measure"
+import { Global, Box, ScrollArea, ScrollContent } from "./styles"
 
 function ScrollBox({ size, color, children }: { size: number | string; color: string; children: any }) {
   const scrollBoxRef = React.useRef<HTMLDivElement | null>(null)
@@ -21,41 +20,49 @@ function ScrollBox({ size, color, children }: { size: number | string; color: st
   )
 }
 
-const MeasuredBox = React.forwardRef(({ children, color }: { children: any; color: string }, ref: any) => {
-  const [big, setBig] = React.useState(false)
+function MeasuredBox({ color }: { color: string }) {
+  // This line is all you need ...
+  const [ref, bounds] = useMeasure({ scroll: true })
+  // The rest is just for effects, hover and mouse tracking
+  const [big, setBig] = useState(false)
+  const [hovered, setHover] = useState(false)
+  const [xy, setXY] = useState([0, 0])
+
   return (
-    <Box ref={ref} onClick={() => setBig(!big)} size={big ? 250 : 200} color={color}>
-      {children}
+    <Box
+      ref={ref}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+      onMouseMove={({ clientX, clientY }) => setXY([clientX, clientY])}
+      onClick={() => setBig(!big)}
+      size={big ? 270 : 235}
+      color={color}>
+      {Object.keys(bounds).map(key => (
+        <Fragment key={key}>
+          <span>{key}</span>
+          <span>{Math.round(bounds[key])}px</span>
+        </Fragment>
+      ))}
+      {hovered && (
+        <>
+          <span>mouse x</span>
+          <span>{Math.round(xy[0] - bounds.left)}px</span>
+          <span>mouse y</span>
+          <span>{Math.round(xy[1] - bounds.top)}px</span>
+        </>
+      )}
     </Box>
   )
-})
+}
 
 function Example() {
-  const [ref, bounds] = useMeasure({ scroll: true })
-  const prev = useRef<RectReadOnly>(bounds)
-  useEffect(() => void (prev.current = { ...bounds }), [bounds])
-
-  const inter = (o: any) => `rgba(0,0,0,${o})`
-  const springs: any = useSpring({
-    reset: true,
-    from: Object.keys(bounds).reduce((acc, key) => ({ ...acc, [key]: prev.current[key] !== bounds[key] ? 1 : 0 }), {}),
-    to: Object.keys(bounds).reduce((acc, key) => ({ ...acc, [key]: 0 }), {}),
-  })
-
   return (
     <>
       <Global color="white" />
-      <div style={{ width: '100vw', height: '300vh', paddingTop: '20vh' }}>
+      <div style={{ width: "100vw", height: "300vh", paddingTop: "20vh" }}>
         <ScrollBox size="60vh" color="#272730">
           <ScrollBox size="50vh" color="#676770">
-            <MeasuredBox ref={ref} color="#F7567C">
-              {Object.keys(bounds).map(key => (
-                <Fragment key={key}>
-                  <span>{key}</span>
-                  <a.span style={{ background: springs[key].interpolate(inter) }}>{Math.round(bounds[key])}px</a.span>
-                </Fragment>
-              ))}
-            </MeasuredBox>
+            <MeasuredBox color="#F7567C" />
           </ScrollBox>
         </ScrollBox>
       </div>
@@ -63,4 +70,4 @@ function Example() {
   )
 }
 
-ReactDOM.render(<Example />, document.getElementById('root'))
+ReactDOM.render(<Example />, document.getElementById("root"))
